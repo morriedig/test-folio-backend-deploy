7e8326a63d3b20c947b06076312e319c90335cee
Boru <r10725008@ntu.edu.tw>
2022-03-24 14:43:00 +0800

    fix: replace secret key and save that in environment file

--- folio_backend/folio_backend/settings.py
+++ folio_backend/folio_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m@d6%05by&$@vxbpi9nuj(&o2c+-d(_pccdamh)ueim19)t)0^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config("SECRET_KEY")
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
