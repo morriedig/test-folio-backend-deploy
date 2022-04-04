a5121551117b5b8082958f45c41f9b8404b89843
chenanchien <kevin19981031@gmail.com>
2022-04-04 22:06:02 +0800

    Feat: database build and insert fake data

--- folio_backend/engine/models.py
+++ folio_backend/engine/models.py
@@ -1,3 +1,84 @@
 # from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Follow(models.Model):
+    fid = models.TextField(primary_key=True)
+    portfolio = models.ForeignKey("Portfolio", models.CASCADE, db_column="portfolio")
+    user = models.ForeignKey("User", models.CASCADE, db_column="user")
+    starttime = models.DateTimeField()
+    endtime = models.DateTimeField()
+    f_budget = models.FloatField()
+    stop_limit = models.FloatField()
+    is_alive = models.BooleanField(default=True)
+
+    class Meta:
+        managed = True
+        db_table = "follow"
+        # unique_together = (("portfolio", "user"),)
+
+
+class Portfolio(models.Model):
+    pid = models.TextField(primary_key=True)
+    name = models.TextField()
+    description = models.TextField()
+    owner = models.ForeignKey("User", models.CASCADE, db_column="owner")
+    p_budget = models.FloatField()
+    is_public = models.BooleanField(default=False)
+    is_alive = models.BooleanField(default=True)
+
+    class Meta:
+        managed = True
+        db_table = "portfolio"
+
+
+class Stock(models.Model):
+    sid = models.TextField(primary_key=True)
+    name = models.TextField()
+    group = models.TextField()
+
+    class Meta:
+        managed = True
+        db_table = "stock"
+
+
+class Stockprice(models.Model):
+    spid = models.TextField(primary_key=True)
+    stock = models.ForeignKey(Stock, models.CASCADE, db_column="stock")
+    time = models.DateTimeField()
+    price = models.FloatField()
+
+    class Meta:
+        managed = True
+        db_table = "stockprice"
+        # unique_together = (("stock", "time"),)
+
+
+class Transaction(models.Model):
+    tid = models.TextField(primary_key=True)
+    portfolio = models.ForeignKey(Portfolio, models.CASCADE, db_column="portfolio")
+    stock = models.ForeignKey(Stock, models.CASCADE, db_column="stock")
+    amount = models.FloatField()
+    time = models.DateTimeField()
+    price = models.FloatField()
+
+    class Meta:
+        managed = True
+        db_table = "transaction"
+        # unique_together = (("portfolio", "stock"),)
+
+
+class User(models.Model):
+    uid = models.TextField(primary_key=True)
+    name = models.TextField()
+    bankaccount = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    u_budget = models.FloatField(blank=True, null=True)
+    id_number = models.TextField()
+
+    class Meta:
+        managed = True
+        db_table = "user"
---
+++ folio_backend/engine/views_test.py
@@ -0,0 +1,111 @@
+import datetime
+
+# from datetime import timedelta
+import pytz
+import twstock
+from django.shortcuts import render
+
+from .models import *
+
+
+def insert_test_data(request):
+    u1 = User(
+        uid="u0001",
+        name="provider1",
+        bankaccount="123456789123",
+        email="r10725021@ntu.edu.tw",
+        password="password",
+        u_budget=10000,
+        id_number="A123456789",
+    )
+    u2 = User(
+        uid="u0002",
+        name="follower1",
+        bankaccount="234567891234",
+        email="b06705004@ntu.edu.tw",
+        password="password",
+        u_budget=5000,
+        id_number="A123456780",
+    )
+    p1 = Portfolio(
+        pid="p0001",
+        name="投資組合1",
+        description="測試用",
+        owner=u1,
+        p_budget=5000,
+        is_public=True,
+        is_alive=True,
+    )
+    s1 = Stock(sid="s0001", name="公司1")
+    u1.save()
+    u2.save()
+    p1.save()
+    s1.save()
+    sp1 = Stockprice(
+        spid="sp0001",
+        stock=Stock.objects.filter(sid="s0001")[0],
+        price=100,
+        time=datetime.datetime(2015, 10, 9, 23, 55, 59),
+    )
+    t1 = Transaction(
+        tid="t0001",
+        portfolio=Portfolio.objects.filter(pid="p0001")[0],
+        stock=Stock.objects.filter(sid="s0001")[0],
+        amount=10,
+        price=100,
+        time=datetime.datetime(2015, 10, 9, 23, 55, 59),
+    )
+    f1 = Follow(
+        fid="f0001",
+        portfolio=Portfolio.objects.filter(pid="p0001")[0],
+        user=User.objects.filter(uid="u0002")[0],
+        starttime=datetime.datetime(2015, 10, 8, 23, 55, 59),
+        endtime=datetime.datetime(2015, 11, 7, 23, 55, 59),
+        f_budget=1000,
+        is_alive=True,
+        stop_limit=0.1,
+    )
+    sp1.save()
+    t1.save()
+    f1.save()
+
+    test_message = "insert fake data done"
+    return render(request, "insert_test.html", locals())
+
+
+def insert_stock(request):
+    twstock.__update_codes()
+    ks = list(twstock.codes.keys())
+    stocks = []
+    for i in range(len(ks)):
+        if twstock.codes[ks[i]].market == "上市" and twstock.codes[ks[i]].type == "股票":
+            stocks.append(twstock.codes[ks[i]])
+    for stock in stocks:
+        s = Stock(sid=stock.code, name=stock.name, group=stock.group)
+        s.save()
+    test_message = "insert stock done"
+    return render(request, "insert_test.html", locals())
+
+
+def insert_stock_price(request):
+    stocks = Stock.objects.all()
+    i = ""
+    for target_stock in stocks:
+        if target_stock.sid == "s0001":
+            continue
+        sid = target_stock.sid
+        stock = twstock.Stock(sid)
+        target_price = stock.fetch_from(2022, 3)  # 取用2022/03至今每天的交易資料
+        stock_data = [[tp.date, tp.close] for tp in target_price]
+        for sd in stock_data:
+            sp = Stockprice(
+                spid=str(target_stock.sid) + str(sd[0].replace(tzinfo=pytz.UTC)),
+                stock=Stock.objects.filter(sid=sid)[0],
+                price=sd[1],
+                time=sd[0].replace(tzinfo=pytz.UTC),
+            )
+            sp.save()
+            i += str(sd[0])
+        break
+    test_message = "insert stock" + sid + "for" + str(i) + "price done"
+    return render(request, "insert_test.html", locals())
