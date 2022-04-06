0ee691a7516436b1bab01f7722608e2f5511e04c
chenanchien <kevin19981031@gmail.com>
2022-04-07 00:32:39 +0800

    Feat:able to generate er diagram

--- folio_backend/folio_backend/settings.py
+++ folio_backend/folio_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "engine"
+    "engine",
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
