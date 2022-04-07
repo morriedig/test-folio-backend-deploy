7608787ec5b953402d3a69cede9106687b17616d
chenanchien <kevin19981031@gmail.com>
2022-04-07 23:03:54 +0800

    Fix: change individual id to default id and alter some columns

--- folio_backend/engine/models.py
+++ folio_backend/engine/models.py
@@ -5,12 +5,13 @@ from django.db import models
 
 
 class Follow(models.Model):
-    fid = models.TextField(primary_key=True)
+    # id = models.BigIntegerField(primary_key = True)
     portfolio = models.ForeignKey("Portfolio", models.CASCADE, db_column="portfolio")
     user = models.ForeignKey("User", models.CASCADE, db_column="user")
     starttime = models.DateTimeField()
     endtime = models.DateTimeField()
-    f_budget = models.FloatField()
+    budget = models.FloatField()
+    cash = models.FloatField()
     stop_limit = models.FloatField()
     is_alive = models.BooleanField(default=True)
 
@@ -21,11 +22,12 @@ class Follow(models.Model):
 
 
 class Portfolio(models.Model):
-    pid = models.TextField(primary_key=True)
+    # id = models.BigIntegerField(primary_key = True)
     name = models.TextField()
     description = models.TextField()
     owner = models.ForeignKey("User", models.CASCADE, db_column="owner")
-    p_budget = models.FloatField()
+    cash = models.FloatField()
+    budget = models.FloatField()
     is_public = models.BooleanField(default=False)
     is_alive = models.BooleanField(default=True)
 
@@ -35,7 +37,8 @@ class Portfolio(models.Model):
 
 
 class Stock(models.Model):
-    sid = models.TextField(primary_key=True)
+    # id = models.AutoField(primary_key = True)
+    code = models.TextField()
     name = models.TextField()
     group = models.TextField()
 
@@ -45,7 +48,7 @@ class Stock(models.Model):
 
 
 class Stockprice(models.Model):
-    spid = models.TextField(primary_key=True)
+    # id = models.BigIntegerField(primary_key = True)
     stock = models.ForeignKey(Stock, models.CASCADE, db_column="stock")
     time = models.DateTimeField()
     price = models.FloatField()
@@ -57,7 +60,7 @@ class Stockprice(models.Model):
 
 
 class Transaction(models.Model):
-    tid = models.TextField(primary_key=True)
+    # id = models.BigIntegerField(primary_key = True)
     portfolio = models.ForeignKey(Portfolio, models.CASCADE, db_column="portfolio")
     stock = models.ForeignKey(Stock, models.CASCADE, db_column="stock")
     amount = models.FloatField()
@@ -71,12 +74,12 @@ class Transaction(models.Model):
 
 
 class User(models.Model):
-    uid = models.TextField(primary_key=True)
+    # id = models.BigIntegerField(primary_key = True)
     name = models.TextField()
     bankaccount = models.TextField()
     email = models.TextField()
     password = models.TextField()
-    u_budget = models.FloatField(blank=True, null=True)
+    # budget = models.FloatField(blank=True, null=True)
     id_number = models.TextField()
 
     class Meta:
--- folio_backend/engine/views_test.py
+++ folio_backend/engine/views_test.py
@@ -1,5 +1,7 @@
 import datetime
+
 import pandas as pd
+
 # from datetime import timedelta
 import pytz
 import twstock
@@ -10,58 +12,54 @@ from .models import *
 
 def insert_test_data(request):
     u1 = User(
-        uid="u0001",
         name="provider1",
         bankaccount="123456789123",
         email="r10725021@ntu.edu.tw",
         password="password",
-        u_budget=10000,
         id_number="A123456789",
     )
     u2 = User(
-        uid="u0002",
         name="follower1",
         bankaccount="234567891234",
         email="b06705004@ntu.edu.tw",
         password="password",
-        u_budget=5000,
         id_number="A123456780",
     )
     p1 = Portfolio(
-        pid="p0001",
         name="投資組合1",
         description="測試用",
         owner=u1,
-        p_budget=5000,
+        budget=5000,
+        cash=5000,
         is_public=True,
         is_alive=True,
     )
-    s1 = Stock(sid="s0001", name="公司1")
+    s1 = Stock(code="s01", name="公司1")
     u1.save()
     u2.save()
     p1.save()
     s1.save()
+    # test_message = Stock.objects.filter(id=1)
+    # return render(request, "insert_test.html", locals())
     sp1 = Stockprice(
-        spid="sp0001",
-        stock=Stock.objects.filter(sid="s0001")[0],
+        stock=Stock.objects.filter(id=1)[0],
         price=100,
-        time=datetime.datetime(2015, 10, 9, 23, 55, 59),
+        time=datetime.datetime(2022, 3, 1, 0, 0, 0),
     )
     t1 = Transaction(
-        tid="t0001",
-        portfolio=Portfolio.objects.filter(pid="p0001")[0],
-        stock=Stock.objects.filter(sid="s0001")[0],
+        portfolio=Portfolio.objects.filter(id=1)[0],
+        stock=Stock.objects.filter(id=1)[0],
         amount=10,
         price=100,
-        time=datetime.datetime(2015, 10, 9, 23, 55, 59),
+        time=datetime.datetime(2022, 3, 1, 23, 55, 59),
     )
     f1 = Follow(
-        fid="f0001",
-        portfolio=Portfolio.objects.filter(pid="p0001")[0],
-        user=User.objects.filter(uid="u0002")[0],
-        starttime=datetime.datetime(2015, 10, 8, 23, 55, 59),
-        endtime=datetime.datetime(2015, 11, 7, 23, 55, 59),
-        f_budget=1000,
+        portfolio=Portfolio.objects.filter(id=1)[0],
+        user=User.objects.filter(id=1)[0],
+        starttime=datetime.datetime(2022, 2, 1, 23, 55, 59),
+        endtime=datetime.datetime(2022, 4, 1, 23, 55, 59),
+        budget=1000,
+        cash=1000,
         is_alive=True,
         stop_limit=0.1,
     )
@@ -74,10 +72,10 @@ def insert_test_data(request):
 
 
 def insert_stock(request):
-    twstocks = pd.read_csv('engine/twse_equities.csv')
-    stocks = twstocks[(twstocks['market'] == "上市") & (twstocks['type'] == "股票")]
-    for i,stock in stocks.iterrows():
-        s = Stock(sid=stock['code'], name=stock['name'], group=stock['group'])
+    twstocks = pd.read_csv("engine/twse_equities.csv")
+    stocks = twstocks[(twstocks["market"] == "上市") & (twstocks["type"] == "股票")]
+    for i, stock in stocks.iterrows():
+        s = Stock(code=stock["code"], name=stock["name"], group=stock["group"])
         s.save()
     test_message = "insert stock done"
     return render(request, "insert_test.html", locals())
@@ -87,22 +85,21 @@ def insert_stock_price(request):
     stocks = Stock.objects.all()
     m = ""
     for i, target_stock in enumerate(stocks):
-        if target_stock.sid == "s0001":
+        if target_stock.code == "s01":
             continue
-        sid = target_stock.sid
-        stock = twstock.Stock(sid)
+        sid = target_stock.id
+        stock = twstock.Stock(target_stock.code)
         target_price = stock.fetch_from(2022, 3)  # 取用2022/03至今每天的交易資料
         stock_data = [[tp.date, tp.close] for tp in target_price]
         for sd in stock_data:
             if sd[1] != None:
                 sp = Stockprice(
-                    spid=str(target_stock.sid) + str(sd[0].replace(tzinfo=pytz.UTC)),
-                    stock=Stock.objects.filter(sid=sid)[0],
+                    stock=Stock.objects.filter(id=sid)[0],
                     price=sd[1],
                     time=sd[0].replace(tzinfo=pytz.UTC),
                 )
                 sp.save()
-        m += str(target_stock.sid) + '\n'
+        m += str(target_stock.code) + "\n"
         if i == 100:
             break
     test_message = "insert stock:\n" + str(m) + "price done"
