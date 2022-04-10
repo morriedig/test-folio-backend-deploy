96c2fba7ce82eb2223ea1a826737ab6c9a4c2671
cu2189191862 <cupid.cu21@gmail.com>
2022-04-10 12:53:13 +0800

    use local db

--- folio_backend/folio_backend/settings.py
+++ folio_backend/folio_backend/settings.py
@@ -76,7 +76,15 @@ WSGI_APPLICATION = "folio_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
