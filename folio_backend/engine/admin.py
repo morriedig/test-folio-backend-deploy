d795ac04ca9650ca713db2b825109d02b4833bfb
cu2189191862 <cupid.cu21@gmail.com>
2022-04-10 12:52:33 +0800

    register models to admin pages

--- folio_backend/engine/admin.py
+++ folio_backend/engine/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from engine.models import Follow, Portfolio, Stock, Stockprice, Transaction, User
+
+admin.site.register(Follow)
+admin.site.register(Portfolio)
+admin.site.register(Stock)
+admin.site.register(Stockprice)
+admin.site.register(Transaction)
+admin.site.register(User)
