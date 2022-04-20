a25b2d9c7612c1d931069829c5bc7482333e8e46
chenanchien <kevin19981031@gmail.com>
2022-04-20 15:53:17 +0800

    refactor: add default value and cash to user and portfolio

--- folio_backend/engine/admin.py
+++ folio_backend/engine/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Portfolio)
+admin.site.register(Stock)
+admin.site.register(Transaction)
+admin.site.register(Stockprice)
+admin.site.register(Follow)
