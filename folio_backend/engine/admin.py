4aff86b51a6e9193f09f37e3f1378787222db791
chengyu.liou <chengyu@TW168020024M.local>
2022-04-27 20:42:56 +0800

    fix: db reconstruct

--- folio_backend/engine/admin.py
+++ folio_backend/engine/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+from IAM.models import MyUser
 
-admin.site.register(User)
-admin.site.register(Portfolio)
-admin.site.register(Stock)
-admin.site.register(Transaction)
-admin.site.register(Stockprice)
-admin.site.register(Follow)
+class idAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+#admin.site.register(MyUser)
+admin.site.register(Portfolio,idAdmin)
+admin.site.register(Stock,idAdmin)
+admin.site.register(Transaction,idAdmin)
+admin.site.register(Stockprice,idAdmin)
+admin.site.register(Follow,idAdmin)
