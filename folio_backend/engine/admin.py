82d04763c28e21dba79ef9a5e15238352c267ec3
chengyu.liou <r10725020@ntu.edu.tw>
2022-04-27 21:47:27 +0800

    fix: black

--- folio_backend/engine/admin.py
+++ folio_backend/engine/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-from IAM.models import MyUser
+
 
 class idAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-#admin.site.register(MyUser)
-admin.site.register(Portfolio,idAdmin)
-admin.site.register(Stock,idAdmin)
-admin.site.register(Transaction,idAdmin)
-admin.site.register(Stockprice,idAdmin)
-admin.site.register(Follow,idAdmin)
+    readonly_fields = ("id",)
+
+
+admin.site.register(Portfolio, idAdmin)
+admin.site.register(Stock, idAdmin)
+admin.site.register(Transaction, idAdmin)
+admin.site.register(Stockprice, idAdmin)
+admin.site.register(Follow, idAdmin)
