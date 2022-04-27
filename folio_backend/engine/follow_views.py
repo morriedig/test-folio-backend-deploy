86816903dddb58d8bd9178251325a52c3c6c4f43
chenanchien <kevin19981031@gmail.com>
2022-04-27 23:49:25 +0800

    fix: make follow api to fit new db

--- folio_backend/engine/follow_views.py
+++ folio_backend/engine/follow_views.py
@@ -2,12 +2,14 @@ from datetime import datetime
 
 from django.db.models import Sum
 from engine.models import *
+from IAM.models import *
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .follow_serializers import Followserializer
+from .utils import *
 
 
 # Create your views here.
@@ -19,7 +21,7 @@ class FollowAPIView(GenericAPIView):
         try:
             data = request.data
             if data["is_follow"] == 1:
-                # user = User.objects.get(id=1)
+                # user = MyUser.objects.get(id=1)
                 cash = float(data["cash"])
                 pid = data["pid"]
                 portfolio = Portfolio.objects.get(id=pid)
@@ -36,13 +38,14 @@ class FollowAPIView(GenericAPIView):
                 )
                 new_follow.save()
                 owner = portfolio.owner
-                target_user = User.objects.filter(id=user.id)
-                target_owner = User.objects.filter(id=owner.id)
+                target_user = MyUser.objects.filter(id=user.id)
+                target_owner = MyUser.objects.filter(id=owner.id)
                 target_user.update(budget=user.budget - portfolio.follow_price - cash)
                 target_owner.update(budget=owner.budget + portfolio.follow_price)
+                return Response("SUCCESS", status=status.HTTP_200_OK)
             elif data["is_follow"] == 0:
                 money = 0.0
-                # user = User.objects.get(id=1)
+                # user = MyUser.objects.get(id=1)
                 pid = data["pid"]
                 portfolio = Portfolio.objects.get(id=pid)
                 follow = Follow.objects.filter(portfolio=portfolio, user=user, is_alive=True)
@@ -51,15 +54,20 @@ class FollowAPIView(GenericAPIView):
                 )
                 for stock in transaction:
                     stockprice = Stockprice.objects.filter(stock=stock["stock"]).order_by("-time")[0]
+                    message += str(stock)
+                    message += ", "
+                    message += str(stock["total_amount"])
+                    message += ", "
+                    message += str(stockprice.price)
+                    message += ", "
                     money += stock["total_amount"] * stockprice.price
 
                 money = money / portfolio.budget * follow[0].budget
-                target_user = User.objects.filter(id=user.id)
+                target_user = MyUser.objects.filter(id=user.id)
                 target_user.update(budget=user.budget + money)
                 follow.update(is_alive=False)
-
                 return Response("UNFOLLOW SUCCESS", status=status.HTTP_200_OK)
 
-            return Response("SUCCESS?", status=status.HTTP_200_OK)
+            return Response("SUCCESS", status=status.HTTP_200_OK)
         except:
             return Response("Maybe somthing WRONG IN REQUEST DATA", status=status.HTTP_400_BAD_REQUEST)
