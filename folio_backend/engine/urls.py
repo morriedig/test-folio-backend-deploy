f4ef7144afa153205511ffd6870c4373bbdba788
chenanchien <kevin19981031@gmail.com>
2022-04-29 22:31:21 +0800

    feat: add api url

--- folio_backend/engine/urls.py
+++ folio_backend/engine/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .follow_views import FollowAPIView
+from .stock_views import StockAPIView
 from .transaction_views import TransactionAPIView
 
 urlpatterns = [
     path("transaction/", TransactionAPIView.as_view()),
     path("follow/", FollowAPIView.as_view()),
+    path("stock/", StockAPIView.as_view()),
 ]
