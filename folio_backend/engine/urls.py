8bbc85b9133ef57413dda55e94e626323ac25605
Boru <r10725008@ntu.edu.tw>
2022-04-30 21:09:55 +0800

    feat: update user api (retrieve, update, partial update)

--- folio_backend/engine/urls.py
+++ folio_backend/engine/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from .follow_views import FollowAPIView
 from .transaction_views import TransactionAPIView
+from .user.views import UserSelfView, UserSpecificView
 
 urlpatterns = [
     path("transaction/", TransactionAPIView.as_view()),
     path("follow/", FollowAPIView.as_view()),
+    path("user/", UserSelfView.as_view()),
+    path("user/<int:pk>/", UserSpecificView.as_view()),
 ]
