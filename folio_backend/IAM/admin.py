c165f8a8b9767bd4cc8e63efed5b789163b7e83b
chengyu.liou <r10725020@ntu.edu.tw>
2022-05-17 00:46:54 +0800

    feat: post,get,patch

--- folio_backend/IAM/admin.py
+++ folio_backend/IAM/admin.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
-from rules.contrib.admin import ObjectPermissionsModelAdmin
 
 from .models import MyUser
 
