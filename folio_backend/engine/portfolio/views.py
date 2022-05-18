08d187d21557e69a8ba5383b053456f2d7d553ee
chengyu.liou <r10725020@ntu.edu.tw>
2022-05-18 16:19:43 +0800

    fix: permission

--- folio_backend/engine/portfolio/views.py
+++ folio_backend/engine/portfolio/views.py
@@ -2,29 +2,31 @@ from datetime import datetime
 
 from engine.models import Portfolio, Stock, Transaction
 from engine.rules import PortfolioCreatePermission, PortfolioRetrievePermission, PortfolioUpdatePermission
+from IAM.models import MyUser
 from IAM.permissions import IsUserInfoCompleted
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 
-from .serializers import PortfolioSerializer
+from .serializers import PortfolioSerializer, PublicPortfolioSerializer
 
 
 # Create your views here.
 class PortfolioAPIView(GenericAPIView, ListModelMixin):
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
-    permission_classes = [IsUserInfoCompleted]
+    permission_classes = []
 
     def get_permissions(self):
         if self.request.method in ["POST"]:
+            self.permission_classes.append(IsUserInfoCompleted)
             self.permission_classes.append(PortfolioCreatePermission)
-        elif self.request.method in ["GET"]:
+        elif self.request.method in ["GET"] and self.request.query_params.get("pid", "") != "":
             self.permission_classes.append(PortfolioRetrievePermission)
         elif self.request.method in ["PATCH"]:
+            self.permission_classes.append(IsUserInfoCompleted)
             self.permission_classes.append(PortfolioUpdatePermission)
-
         return [permission() for permission in self.permission_classes]
 
     def get(self, request, *args, **kargs):
@@ -32,15 +34,19 @@ class PortfolioAPIView(GenericAPIView, ListModelMixin):
         # order_by = request.query_params.get("order", "follow")
         try:
             if pid == "":
-                portfolio = Portfolio.objects.filter().all()
+                portfolio = Portfolio.objects.filter(is_public=True, is_alive=True).all()
             else:
                 portfolio = Portfolio.objects.filter(id=pid)
         except:
-            return Response("PORTFOLIO " + str(pid) + " DOES NOT EXIST", status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                "PORTFOLIO " + str(pid) + " DOES NOT EXIST OR DISCARDED OR PRIVATE", status=status.HTTP_400_BAD_REQUEST
+            )
         for p in portfolio:
             self.check_object_permissions(self.request, p)
-        serializer = PortfolioSerializer(portfolio, many=True, context={"user": request.user})
-
+        if isinstance(request.user, MyUser):
+            serializer = PortfolioSerializer(portfolio, many=True, context={"user": request.user})
+        else:
+            serializer = PublicPortfolioSerializer(portfolio, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
         # return Response("no auth or no profolio", status=status.HTTP_400_BAD_REQUEST)
 
@@ -84,6 +90,7 @@ class PortfolioAPIView(GenericAPIView, ListModelMixin):
         return Response("SUCCESS", status=status.HTTP_200_OK)
 
     def patch(self, request, *args, **kargs):
+
         data = request.data
         if "pid" not in data:
             return Response("MISSING pid IN REQUEST", status=status.HTTP_400_BAD_REQUEST)
